636380aac2972ae9586a97aa8868543db458f839
Onur Ulusoy <onurulusoys4@gmail.com>
2022-07-21 02:09:22 +0300

    Refactor DriveMotors for performance and maint.

--- Core/Inc/motordrive.h
+++ Core/Inc/motordrive.h
@@ -2,15 +2,13 @@
 #define MOTOR_DRIVE_H
 
 #include "proto/warp_protocol.pb.h" //generated using warp_protocol.proto
-// #include "pb_decode.h"
-// #include "pb_encode.h"
-
 #include "tim.h"
+
 #define PULSE_CONSTANT 1.68
+#define CHANNEL_COUNT 8
 
 WarpCommand esc_command;
 
 void DriveMotors(const WarpCommand command);
-
 
 #endif //MOTOR_DRIVE_H
--- Core/Src/motordrive.c
+++ Core/Src/motordrive.c
@@ -1,39 +1,27 @@
 #include "motordrive.h"
 
+#define CHANNEL_COUNT 8
+
+struct ChannelConfig {
+    uint16_t command_value;
+    TIM_HandleTypeDef* timer;
+    uint32_t channel;
+};
+
 void DriveMotors(const WarpCommand command) {
+    struct ChannelConfig configs[CHANNEL_COUNT] = {
+        {command.esc_pwm_ch0, &htim2, TIM_CHANNEL_1},
+        {command.esc_pwm_ch1, &htim2, TIM_CHANNEL_2},
+        {command.esc_pwm_ch2, &htim3, TIM_CHANNEL_1},
+        {command.esc_pwm_ch3, &htim3, TIM_CHANNEL_2},
+        {command.esc_pwm_ch4, &htim4, TIM_CHANNEL_1},
+        {command.esc_pwm_ch5, &htim4, TIM_CHANNEL_2},
+        {command.esc_pwm_ch6, &htim4, TIM_CHANNEL_3},
+        {command.esc_pwm_ch7, &htim4, TIM_CHANNEL_4},
+    };
 
-  uint16_t pulse;
-
-  const uint16_t ch0_command = command.esc_pwm_ch0;
-  pulse = (int) round(PULSE_CONSTANT * ch0_command);
-  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pulse);
-
-  const uint16_t ch1_command = command.esc_pwm_ch1;
-  pulse = (int) round(PULSE_CONSTANT * ch1_command);
-  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, pulse);
-
-  const uint16_t ch2_command = command.esc_pwm_ch2;
-  pulse = (int) round(PULSE_CONSTANT * ch2_command);
-  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pulse);
-
-  const uint16_t ch3_command = command.esc_pwm_ch3;
-  pulse = (int) round(PULSE_CONSTANT * ch3_command);
-  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pulse);
-
-  const uint16_t ch4_command = command.esc_pwm_ch4;
-  pulse = (int) round(PULSE_CONSTANT * ch4_command);
-  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, pulse);
-
-  const uint16_t ch5_command = command.esc_pwm_ch5;
-  pulse = (int) round(PULSE_CONSTANT * ch5_command);
-  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, pulse);
-
-  const uint16_t ch6_command = command.esc_pwm_ch6;
-  pulse = (int) round(PULSE_CONSTANT * ch6_command);
-  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, pulse);
-
-  const uint16_t ch7_command = command.esc_pwm_ch7;
-  pulse = (int) round(PULSE_CONSTANT * ch7_command);
-  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, pulse);
-
+    for (int i = 0; i < CHANNEL_COUNT; i++) {
+        uint16_t pulse = (int) round(PULSE_CONSTANT * configs[i].command_value);
+        __HAL_TIM_SET_COMPARE(configs[i].timer, configs[i].channel, pulse);
+    }
 }
