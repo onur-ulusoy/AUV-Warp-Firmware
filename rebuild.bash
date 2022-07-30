0ed4f654c6629267e610e1cd43eba9e2c5a6961f
Onur Ulusoy <onurulusoys4@gmail.com>
2022-07-30 22:49:54 +0300

    develop rebuild.bash to increase the quality of output

--- rebuild.bash
+++ rebuild.bash
@@ -16,11 +16,13 @@ make clean
 # Building the project
 echo -e "\033[1;33m[INFO] Building the project...\033[0m"
 if [ $VERBOSE -eq 1 ]; then
-    # If verbose flag is provided, print warnings and errors
+    # If verbose flag is provided, print warnings, errors, and the final output
     make all &> build.log
     MAKE_EXIT_CODE=$?
-    # Print warnings and errors
-    grep --color -E "warning:|error:" build.log
+    # Print warnings, errors, and the final output
+    grep --color=always -E "warning:|error:" build.log | sed 's/warning:/\o033[0;33m&\o033[0m/g' | sed 's/error:/\o033[0;31m&\o033[0m/g'
+    # Print the last 4 lines
+    tail -4 build.log
 else
     # Otherwise, print all output
     make all
